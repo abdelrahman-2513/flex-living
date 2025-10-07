cc1f210dba83717c33f86b4aa82c12e3416ee3b2
abdelrahman-2513 <ab19622001@gmail.com>
2025-10-07 16:25:25 +0300

    remove un wanted data from config

--- backend/src/config/configuration.ts
+++ backend/src/config/configuration.ts
@@ -2,10 +2,10 @@ export default () => ({
   port: parseInt(process.env.PORT || '3001', 10),
   hostaway: {
     apiUrl: process.env.HOSTAWAY_API_URL || 'https://api.hostaway.com/v1',
-    accountId: process.env.HOSTAWAY_ACCOUNT_ID || '61148',
+    accountId: process.env.HOSTAWAY_ACCOUNT_ID || 'your-account-id',
     apiKey:
       process.env.HOSTAWAY_API_KEY ||
-      'f94377ebbbb479490bb3ec364649168dc443dda2e4830facaf5de2e74ccc9152',
+      'your-api-key',
   },
   cors: {
     origins: (process.env.CORS_ORIGINS || 'http://localhost:5173,http://localhost:3000').split(','),
